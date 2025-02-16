3473e1103d44d0a1e0592c470940bd240a073ddf
Codrin Mares <codrin.mares@gmail.com>
2025-02-16 14:31:15 +0200

    Add daisy ui for styles, downgrade to tailwindcss3 for support, add vscode workspace rules and move old code to separate dir.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<!-- <button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button> -->
+
+<button class="bg-teal btn btn-primary">One</button>
+<button class="btn btn-secondary">Two</button>
+<button class="btn-accen btn btn-outline p-4 pt-2">Three</button>
